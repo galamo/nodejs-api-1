2972144e7795be718ad7c4229b0e6c44269d4ace
noUser <noUser>
2019-11-04 19:01:23 +0200

    validation

--- index.js
+++ index.js
@@ -2,22 +2,39 @@
 const express = require("express")
 const data = require("./data.json")
 const logger = require("jbapp-logger");
+const { isValidDates } = require("./utils")
 const api = express()
 
+// const currentDate = moment(1572886068504);
+// console.log(currentDate.format("DD-MM-YY hh:mm:ss"))
+// console.log(currentDate.isValid())
 const port = 4000;
 
 // url for entry point
 // 1. req
 // 2. res 
 // 3. next
+
+
+
 api.get("/message", (req, res, next) => {
     console.log("start req...", req.ip)
     //query params
-    const { product, amount } = req.query;
-    if (!product || !amount) return res.send("You are wrong! , please try again!!")
-    console.log(product + " " + amount)
-    return res.send(`User Details: ${product} , ${amount}`);
+    const { userName, password } = req.query;
+    if (!userName || !password) return res.send("You are wrong! , please try again!!")
+    console.log(userName + " " + password)
+    return res.send(`User Details: ${userName} , ${password}`);
 })
+
+api.get("/booking", (req, res, next) => {
+    const { from, to, dest } = req.query;
+    if (!isValidDates([from, to])) return res.send("error")
+    if (!from || !dest || !to) return res.send("You are wrong! , please try again!!")
+    return res.send(`You booked: ${dest} ,  from: ${from} to: ${to}`);
+})
+
+
+
 
 api.get("/register-to-class", (req, res, next) => {
 
---
+++ utils/index.js
@@ -0,0 +1,14 @@
+const moment = require("moment")
+
+
+function isValidDates(dates) {
+    return !dates.some((date) => {
+        const mDate = moment(date);
+        if (!mDate.isValid()) return true
+    })
+
+}
+
+module.exports = {
+    isValidDates
+}
